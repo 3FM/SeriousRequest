5b6ba61389d455d33727859cbee128655256a30e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2013-11-09 13:00:42 +0100

    js

--- client/index.js
+++ client/index.js
@@ -1,5 +1,22 @@
+function Participant(id) {
+  this.id = id;
+}
 
-console.log($);
+function Campaigns(collectionid) {
+  this.collectionid = collectionid;
+
+}
+Campaigns.prototype.getParticipants = function(callback)  {
+  $.get(
+    "/EventOnWidgetService.asmx/GetActiveCampaignIds?collectionId=" + this.collectionid
+    , function(data) {
+      var result = [];
+
+    callback($(data).find("int").map(function(o, value) { return $(value).html();}));
+  });
+};
+
+
 $(function() {
   var map = L.map('map').setView([51.505, -0.09], 13);
   L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
@@ -7,6 +24,6 @@ $(function() {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
   }).addTo(map);
 
-
-  console.log(map);
+  var campaigns = new Campaigns(4);
+  console.log(campaigns.getParticipants());
 });
